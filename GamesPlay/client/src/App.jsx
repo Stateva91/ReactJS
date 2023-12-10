0e614cb551e47dfa7dfdd79af5532ddea8771597
Snezhana Stateva <114188863+Stateva91@users.noreply.github.com>
2023-12-10 14:13:46 +0100

    add create login page

--- GamesPlay/client/src/App.jsx
+++ GamesPlay/client/src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header/Header"
 import Home from "./components/home/Home"
 import GameList from "./components/game-list/GameList"
 import GameCreate from "./components/game-create/GameCreate"
+import Login from "./components/Login/Login"
 
 
 function App() {
@@ -16,6 +17,8 @@ function App() {
      <Route path="/" element={<Home/>}></Route>
      <Route path="/games" element={<GameList/>}></Route>
      <Route path="/games/create" element={<GameCreate/>}></Route>
+     <Route path="/login" element={<Login/>}></Route>
+     
 
     </Routes>
     
