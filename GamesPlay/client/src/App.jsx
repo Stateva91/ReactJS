fe08c1490608d94678dfc03a73ab44f477e9ba79
Snezhana Stateva <114188863+Stateva91@users.noreply.github.com>
2023-12-10 14:06:33 +0100

    Add create game page

--- GamesPlay/client/src/App.jsx
+++ GamesPlay/client/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom"
 import Header from "./components/Header/Header"
 import Home from "./components/home/Home"
 import GameList from "./components/game-list/GameList"
+import GameCreate from "./components/game-create/GameCreate"
 
 
 function App() {
@@ -14,6 +15,8 @@ function App() {
     <Routes>
      <Route path="/" element={<Home/>}></Route>
      <Route path="/games" element={<GameList/>}></Route>
+     <Route path="/games/create" element={<GameCreate/>}></Route>
+
     </Routes>
     
     </div>
