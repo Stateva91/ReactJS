5e574be2871d69a6b92e711c20e8d0b08c07343e
Snezhana Stateva <114188863+Stateva91@users.noreply.github.com>
2023-12-10 17:08:50 +0100

    Save comments

--- GamesPlay/client/src/components/game-details/GameDetails.jsx
+++ GamesPlay/client/src/components/game-details/GameDetails.jsx
@@ -4,76 +4,82 @@ import { useParams } from "react-router-dom";
 import * as gameService from '../../services/gameService';
 import * as commentService from '../../services/commentService'
 
-export default function GameDetails(){
-    const [game, setGame]=useState({});
-    const {gameId}=useParams();
+export default function GameDetails() {
+    const [game, setGame] = useState({});
+    const [comments, setComments] = useState([]);
+    const { gameId } = useParams();
 
-    useEffect(()=>{
+    useEffect(() => {
         gameService.getOne(gameId)
-        .then(setGame)
+            .then(setGame);
+
+        commentService.getAll()
+            .then(setComments);
     }, [gameId]);
 
-    const addCommentHandler=async(e)=>{
+    const addCommentHandler = async (e) => {
         e.preventDefault();
-      const formData= new FormData(e.currentTarget);
-      const newComment= await commentService.create(
-        gameId,
-        formData.get('username'),
-        formData.get('comment')
-         );
-         console.log(newComment);
+        const formData = new FormData(e.currentTarget);
+        const newComment = await commentService.create(
+            gameId,
+            formData.get('username'),
+            formData.get('comment')
+        );
+        console.log(newComment);
     }
-    return(
+    return (
         <section id="game-details">
-        <h1>Game Details</h1>
-        <div className="info-section">
+            <h1>Game Details</h1>
+            <div className="info-section">
 
-            <div className="game-header">
-                <img className="game-img" src="images/MineCraft.png" />
-                <h1>Bright</h1>
-                <span className="levels">MaxLevel: 4</span>
-                <p className="type">Action, Crime, Fantasy</p>
-            </div>
+                <div className="game-header">
+                    <img className="game-img" src="images/MineCraft.png" />
+                    <h1>Bright</h1>
+                    <span className="levels">MaxLevel: 4</span>
+                    <p className="type">Action, Crime, Fantasy</p>
+                </div>
 
-            <p className="text">
-                Set in a world where fantasy creatures live side by side with humans. A human cop is forced to work
-                with an Orc to find a weapon everyone is prepared to kill for. Set in a world where fantasy
-                creatures live side by side with humans. A human cop is forced
-                to work with an Orc to find a weapon everyone is prepared to kill for.
-            </p>
+                <p className="text">
+                    Set in a world where fantasy creatures live side by side with humans. A human cop is forced to work
+                    with an Orc to find a weapon everyone is prepared to kill for. Set in a world where fantasy
+                    creatures live side by side with humans. A human cop is forced
+                    to work with an Orc to find a weapon everyone is prepared to kill for.
+                </p>
 
-            
-            <div className="details-comments">
-                <h2>Comments:</h2>
-                <ul>
+
+                <div className="details-comments">
+                    <h2>Comments:</h2>
+                    <ul>
+                        {comments.map(({username, text}) => {
+
+                            <li className="comment">
+                                <p>{username}: {text}</p>
+                            </li>
+                        })}
+
+                    </ul>
+                     {comments.length===0 &&(
+                        <p className="no-comment">No comments.</p>
+                     )}
                     
-                    <li className="comment">
-                        <p>Content: I rate this one quite highly.</p>
-                    </li>
-                    <li className="comment">
-                        <p>Content: The best game.</p>
-                    </li>
-                </ul>
-               
-                <p className="no-comment">No comments.</p>
-    </div>
+                </div>
 
-            {/* <!-- Edit/Delete buttons ( Only for creator of this game )  -->
+                {/* <!-- Edit/Delete buttons ( Only for creator of this game )  -->
             <div className="buttons">
                 <a href="#" className="button">Edit</a>
                 <a href="#" className="button">Delete</a>
     </div>*/}
-        </div>
+            </div>
 
-        
-        <article className="create-comment">
-            <label>Add new comment:</label>
-            <form className="form" onSubmit={addCommentHandler}> 
-            <input type="text" name="username" placeholder="username" />
-                <textarea name="comment" placeholder="Comment......"></textarea>
-                <input className="btn submit" type="submit" value="Add Comment"/>
-            </form>
-        </article>
-    </section>
+
+            <article className="create-comment">
+                <label>Add new comment:</label>
+                <form className="form" onSubmit={addCommentHandler}>
+                    <input type="text" name="username" placeholder="username" />
+                    <textarea name="comment" placeholder="Comment......"></textarea>
+                    <input className="btn submit" type="submit" value="Add Comment" />
+                </form>
+            </article>
+        </section>
     );
 }
