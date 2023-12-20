e13b225b539cbbe87a92c66d81ab6281c7b52ed6
Snezhana Stateva <114188863+Stateva91@users.noreply.github.com>
2023-12-20 21:38:19 +0100

    Migrate to collection from jsonstore

--- GamesPlay/client/src/services/gameService.js
+++ GamesPlay/client/src/services/gameService.js
@@ -1,28 +1,43 @@
 import * as request from "../lib/request";
 
-const baseUrl='http://localhost:3030/jsonstore/games'
+const baseUrl = 'http://localhost:3030/data/games'
 
-export const getAll=async ()=>{
-  const result= await request.get(baseUrl);
- 
-  return Object.values(result);
-}
+export const getAll = async () => {
+    const result = await request.get(baseUrl);
 
-export const getOne=async(gameId)=>{
-    const result=await request.get(`${baseUrl}/${gameId}`);
+    return result;
+};
+
+export const getOne = async (gameId) => {
+    const result = await request.get(`${baseUrl}/${gameId}`, );
+
     return result;
 }
 
-export const create= async(gameData)=>{
-   const response= await fetch(baseUrl, {
-    method:'POST',
-    headers: {
-        'content-type': 'application/json'
-    },
-    body: JSON.stringify(gameData)
-   });
+export const getLatest = async () => {
+    // const query = new URLSearchParams({
+    //     sortBy: `_createdOn desc`,
+    //     offset: 0,
+    //     pageSize: 3,
+    // });
 
-   const result=await response.json();
+    const query = encodeURIComponent(`offset=0&pageSize=3`);
+    console.log(query);
+    const result = await request.get(`${baseUrl}?sortBy=_createdOn%20desc&${query}`);
 
-   return result;
+    return result;
+}
+
+export const create = async (gameData) => {
+    const result = await request.post(baseUrl, gameData);
+
+    return result;
 };
+
+export const edit = async (gameId, gameData) => {
+    const result = await request.put(`${baseUrl}/${gameId}`, gameData);
+
+    return result;
+};
+
+export const remove = async (gameId) => request.remove(`${baseUrl}/${gameId}`);
