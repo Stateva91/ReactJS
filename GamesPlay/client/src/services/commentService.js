6e9500ca4a1bfbd54779a272bb0644095680bc1e
Snezhana Stateva <114188863+Stateva91@users.noreply.github.com>
2023-12-20 21:59:52 +0100

    Add comments without username

--- GamesPlay/client/src/services/commentService.js
+++ GamesPlay/client/src/services/commentService.js
@@ -4,21 +4,18 @@ const baseUrl = 'http://localhost:3030/data/comments';
 
 export const getAll = async (gameId) => {
     const query = new URLSearchParams({
-        where: `gameId="${gameId}"`
+        where: `gameId="${gameId}"`,
+        load: `owner=_ownerId:users`,
     });
 
     const result = await request.get(`${baseUrl}?${query}`);
 
-    // TODO: temp solution until migration to collections service 
-   // return Object.values(result).filter(comment => comment.gameId === gameId);
- return result;
-
+    return result;
 };
 
-export const create = async (gameId, username, text) => {
+export const create = async (gameId, text) => {
     const newComment = await request.post(baseUrl, {
         gameId,
-        username, 
         text,
     });
 
